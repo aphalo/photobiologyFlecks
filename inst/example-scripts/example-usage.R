1add7ab3221b3e46c6e2dd2d7f45de93040e8fff
Pedro Aphalo <pedro.aphalo@helsinki.fi>
2026-04-21 16:45:02 +0300

    Add script and data from Maxime

---
+++ inst/example-scripts/example-usage.R
@@ -0,0 +1,27 @@
+library(photobiologyFlecks)
+
+# Taken on 2021.07.17 in the oat fields of Helsinki university
+file.path <- system.file("extdata", "example-oat.csv", package = "photobiologyFlecks")
+# Read the file
+my_data <- read.csv(file_path)
+
+df <- read.csv(file.path, row.names = 1)
+
+df1 <- df[df$height == "low",]
+df2 <- df[df$height == "mid",]
+df3 <- df[df$height == "top",]
+
+dfX <- df1
+Z <- find_zeros(time = dfX$Time,
+                var = dfX$PAR_q)
+dfS <- find_flecks(time = dfX$Time,
+                   var = dfX$PAR_q,
+                   zeroes = Z,
+                   minTime = 0, minAmp = 5, minPdiff = 0.05,
+                   shadeflecks = FALSE,
+                   asmMethod = "max",
+                   verbose = FALSE)
+plot_ts_fleck(time = dfX$Time,
+              var = dfX$PAR_q,
+              zeroes = Z,
+              fleck.data = dfS)
