bd2813721fa1389a930365683d1dc44a29fff139
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2026-08-10 17:21:16 +0300

    Add off-line benchmarking of split_chunks() and group_chunks()

--- inst-not/performance/split-chuncks-benchmark.R
+++ inst-not/performance/split-chuncks-benchmark.R
@@ -7,6 +7,13 @@ library(collapse)
 # performance is much better when time is a numeric variable
 # three_chunks.tb[["time"]] <- as.numeric(three_chunks.tb[["time"]])
 
+
+# BENCHMARKING ------------------------------------------------------------
+
+
+# split_chunks() ----------------------------------------------------------
+
+
 microbenchmark(chunks_diffs.ls <- split_chunks(three_chunks.tb,
                                                time.name = "time",
                                                qty.name = NULL,
@@ -45,49 +52,111 @@ microbenchmark(chunks_no_diffs.ls <- split_chunks(three_chunks.tb,
                                                   verbose = FALSE)
 )
 
-microbenchmark(chunks_no_loop.ls <- group_chunks(three_chunks.tb,
-                                                 time.name = "time",
-                                                 qty.name = NULL,
-                                                 chunk.min.time = 0.051,
-                                                 chunk.min.rows = 1.8e4,
-                                                 verbose = FALSE,
-                                                 na.rm = TRUE,
-                                                 returned.value = "all")
+
+# group_chunks() ----------------------------------------------------------
+
+
+microbenchmark(chunks_time_idx.ls <- group_chunks(three_chunks.tb,
+                                                  time.name = "time",
+                                                  qty.name = NULL,
+                                                  chunk.min.time = 0.051,
+                                                  chunk.min.rows = 1.8e4,
+                                                  verbose = FALSE,
+                                                  na.rm = TRUE,
+                                                  returned.value = "all")
 )
 
-microbenchmark(chunks_no_loop.ls <- group_chunks(three_chunks.tb,
-                                                 time.name = "time",
-                                                 qty.name = NULL,
-                                                 chunk.min.time = 0.051,
-                                                 chunk.min.rows = 1.8e4,
-                                                 verbose = FALSE,
-                                                 na.rm = FALSE,
-                                                 returned.value = "all")
+microbenchmark(chunks_time_idx.ls <- group_chunks(three_chunks.tb,
+                                                  time.name = "time",
+                                                  qty.name = NULL,
+                                                  chunk.min.time = 0.051,
+                                                  chunk.min.rows = 1.8e4,
+                                                  verbose = FALSE,
+                                                  na.rm = FALSE,
+                                                  returned.value = "all")
 )
 
-microbenchmark(group.ids <- group_chunks(three_chunks.tb,
-                                         time.name = "time",
-                                         qty.name = NULL,
-                                         chunk.min.time = 0.051,
-                                         chunk.min.rows = 1.8e4,
-                                         verbose = FALSE,
-                                         na.rm = TRUE,
-                                         returned.value = "group.idxs")
+microbenchmark(group.idxs <- group_chunks(three_chunks.tb,
+                                          time.name = "time",
+                                          qty.name = NULL,
+                                          chunk.min.time = 0.051,
+                                          chunk.min.rows = 1.8e4,
+                                          verbose = FALSE,
+                                          na.rm = TRUE,
+                                          returned.value = "chunk.idxs")
 )
 
-microbenchmark(group.ids <- group_chunks(three_chunks.tb,
-                                         time.name = "time",
-                                         qty.name = NULL,
-                                         chunk.min.time = 0.051,
-                                         chunk.min.rows = 1.8e4,
-                                         verbose = FALSE,
-                                         na.rm = FALSE,
-                                         returned.value = "group.idxs")
+microbenchmark(group.idxs <- group_chunks(three_chunks.tb,
+                                          time.name = "time",
+                                          qty.name = NULL,
+                                          chunk.min.time = 0.051,
+                                          chunk.min.rows = 1.8e4,
+                                          verbose = FALSE,
+                                          na.rm = FALSE,
+                                          returned.value = "chunk.idxs")
 )
+
+
+# Summaries ---------------------------------------------------------------
+
 
 microbenchmark(fmean(three_chunks.tb, g = group.ids))
 
+
+# PROFILING ---------------------------------------------------------------
+
 library(profvis)
+
+# split_chunks() ----------------------------------------------------------
+
+
+for (i in 1:100) {
+  chunks.ls <- split_chunks(three_chunks.tb,
+                            time.name = "time",
+                            qty.name = NULL,
+                            chunk.min.time = 0.051,
+                            chunk.min.rows = 1.8e4,
+                            add.diffs = TRUE,
+                            verbose = FALSE,
+                            na.rm = TRUE)
+}
+
+for (i in 1:100) {
+  group.ids <- split_chunks(three_chunks.tb,
+                            time.name = "time",
+                            qty.name = NULL,
+                            chunk.min.time = 0.051,
+                            chunk.min.rows = 1.8e4,
+                            add.diffs = TRUE,
+                            verbose = FALSE,
+                            na.rm = FALSE)
+}
+
+for (i in 1:100) {
+  chunks.ls <- split_chunks(three_chunks.tb,
+                            time.name = "time",
+                            qty.name = NULL,
+                            chunk.min.time = 0.051,
+                            chunk.min.rows = 1.8e4,
+                            add.diffs = FALSE,
+                            verbose = FALSE,
+                            na.rm = TRUE)
+}
+
+for (i in 1:100) {
+  group.ids <- split_chunks(three_chunks.tb,
+                            time.name = "time",
+                            qty.name = NULL,
+                            chunk.min.time = 0.051,
+                            chunk.min.rows = 1.8e4,
+                            add.diffs = FALSE,
+                            verbose = FALSE,
+                            na.rm = FALSE)
+}
+
+
+# group_chunks() ----------------------------------------------------------
+
 
 for (i in 1:1000) {
   group.ids <- group_chunks(three_chunks.tb,
@@ -97,7 +166,7 @@ for (i in 1:1000) {
                             chunk.min.rows = 1.8e4,
                             verbose = FALSE,
                             na.rm = TRUE,
-                            returned.value = "group.idxs")
+                            returned.value = "chunk.idxs")
 }
 
 for (i in 1:1000) {
@@ -108,5 +177,27 @@ for (i in 1:1000) {
                             chunk.min.rows = 1.8e4,
                             verbose = FALSE,
                             na.rm = FALSE,
-                            returned.value = "group.idxs")
+                            returned.value = "chunk.idxs")
+}
+
+for (i in 1:1000) {
+  group.ids <- group_chunks(three_chunks.tb,
+                            time.name = "time",
+                            qty.name = NULL,
+                            chunk.min.time = 0.051,
+                            chunk.min.rows = 1.8e4,
+                            verbose = FALSE,
+                            na.rm = TRUE,
+                            returned.value = "chunk.times")
+}
+
+for (i in 1:1000) {
+  group.ids <- group_chunks(three_chunks.tb,
+                            time.name = "time",
+                            qty.name = NULL,
+                            chunk.min.time = 0.051,
+                            chunk.min.rows = 1.8e4,
+                            verbose = FALSE,
+                            na.rm = FALSE,
+                            returned.value = "chunk.times")
 }
